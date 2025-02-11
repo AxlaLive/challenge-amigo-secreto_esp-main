f0066c748d8f9e203ea08076abb322f0329f3307
AxlaLive <alexagarav@hotmail.com>
2025-02-10 21:57:02 -0500

    Agregando variables y primer intento de generar la lista de amigos.

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Variables //
+
+const nombres = [];
+const input = document.getElementById("amigo");
+const listaAmigos = document.getElementById("listaAmigos");
+const agregarBoton = document.getElementById("agregarAmigo");
+const seleccionarBoton = document.getElementById("sortearAmigo");
+const seleccionarNombre = document.getElementById("Sortear amigo secreto");
+
+
+//Se agregan los nombres a la lista//
+agregarBoton.addEventListener("click", function () {
+    const nombres = input.nodeValue.trim();
+    if (nombres !== "") {
+        nombres.push(nombres);
+        
+        const li = document.createElement("li");
+        li.textContent = nombres;
+        listaAmigos.appendChild(li);
+
+        input.nodeValue = "";
+
+    }
+});
