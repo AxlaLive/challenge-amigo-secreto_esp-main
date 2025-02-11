22b7ddcbadb8a27a705246ffbbc72410441af6f1
AxlaLive <alexagarav@hotmail.com>
2025-02-11 05:42:14 -0500

    Segundo intento de generar la lista de amigos

--- app.js
+++ app.js
@@ -3,22 +3,23 @@
 const nombres = [];
 const input = document.getElementById("amigo");
 const listaAmigos = document.getElementById("listaAmigos");
-const agregarBoton = document.getElementById("agregarAmigo");
-const seleccionarBoton = document.getElementById("sortearAmigo");
-const seleccionarNombre = document.getElementById("Sortear amigo secreto");
+const resultadoLista = document.getElementById("resultado");
+const agregarBoton = document.getElementById("agregarAmigo()");
+const seleccionarBoton = document.getElementById("sortearAmigo()");
 
-
+if (!input || !listaAmigos || !agregarBoton){
+    console.error("No se encontraron los elementos en el DOM");
+    return;
+}
 //Se agregan los nombres a la lista//
-agregarBoton.addEventListener("click", function () {
-    const nombres = input.nodeValue.trim();
-    if (nombres !== "") {
-        nombres.push(nombres);
-        
-        const li = document.createElement("li");
-        li.textContent = nombres;
+agregarBoton.addEventListener ("click", function() {
+    let nombre = input.value.trim();
+    if (nombre !== "") {
+        let li =document.createElement("li");
+        li.textContent = nombre;
         listaAmigos.appendChild(li);
-
-        input.nodeValue = "";
-
+        input.value = "";
+    } else {
+        alert("Por favor, ingresa un nombre válido");
     }
 });
